0be373531c3c44367421382d49354f1eb2f398b1
Stefano Moia <s.moia@bcbl.eu>
2021-03-19 19:00:53 +0100

    Move signal related functions to signal module

--- phys2cvr/phys2cvr.py
+++ phys2cvr/phys2cvr.py
@@ -11,9 +11,8 @@ import matplotlib.pyplot as plt
 import scipy.interpolate as spint
 import scipy.stats as sct
 from peakdet.io import load_physio
-from scipy.signal import butter, filtfilt
 
-from phys2cvr import io, _version
+from phys2cvr import io, signal, _version
 from phys2cvr.cli.run import _get_parser
 
 
@@ -46,118 +45,6 @@ def save_bash_call(outdir):
     f = open(os.path.join(log_path, f'p2c_call_{isotime}.sh'), "a")
     f.write(f'#!bin/bash \n{call_str}')
     f.close()
-
-
-def create_hrf(freq=40):
-    """
-    Create a canonical haemodynamic response function sampled at the given frequency.
-
-    Parameters
-    ----------
-    freq: float
-        Sampling frequency of the haemodynamic response function.
-
-    Returns
-    -------
-    hrf: np.ndarray
-        Haemodynamic response function.
-
-    """
-    # Create HRF
-    RT = 1/freq
-    fMRI_T = 16
-    p = [6, 16, 1, 1, 6, 0, 32]
-
-    # Modelled hemodynamic response function - {mixture of Gammas}
-    dt = RT / fMRI_T
-    u = np.arange(0, p[6]/dt+1, 1) - p[5]/dt
-    a1 = p[0] / p[2]
-    b1 = 1 / p[3]
-    a2 = p[1] / p[3]
-    b2 = 1 / p[3]
-    hrf = (sct.gamma.pdf(u*dt, a1, scale=b1) - sct.gamma.pdf(u*dt, a2, scale=b2)/p[4])/dt
-    time_axis = np.arange(0, int(p[6]/RT+1), 1) * fMRI_T
-    hrf = hrf[time_axis]
-    min_hrf = 1e-9*min(hrf[hrf > 10*np.finfo(float).eps])
-
-    if min_hrf < 10*np.finfo(float).eps:
-        min_hrf = 10*np.finfo(float).eps
-
-    hrf[hrf == 0] = min_hrf
-    hrf = hrf/max(hrf)
-
-    plt.figure()
-    plt.plot(time_axis, hrf)
-
-    return hrf
-
-
-def filter_signal(data, tr, lowcut='', highcut=''):
-    """
-    Create a bandpass filter given a lowcut and a highcut, then filter data.
-
-    Parameters
-    ----------
-    data: np.ndarray
-        Data to filter (along last axis)
-    tr: float
-        TR of functional files
-    lowcut: float
-        Lower frequency in the bandpass
-    highcut: float
-        Higher frequency in the bandpass
-
-    Returns
-    -------
-    filt_data: np.ndarray
-        Input `data`, but filtered.
-
-    """
-    if not lowcut:
-        lowcut = 0.02
-    if not highcut:
-        highcut = 0.04
-    nyq = (1 / tr) / 2
-    low = lowcut / nyq
-    high = highcut / nyq
-    a, b = butter(9, [low, high], btype='band')
-    filt_data = filtfilt(a, b, data, axis=-1)
-    return filt_data
-
-
-def convolve_petco2(co2, pidx, freq, outname):
-    # Extract PETco2
-    hrf = create_hrf(freq)
-    co2_lenght = len(co2)
-    nx = np.linspace(0, co2_lenght, co2_lenght)
-    f = spint.interp1d(pidx, co2[pidx], fill_value='extrapolate')
-    petco2 = f(nx)
-
-    # Plot PETco2
-    plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-    plt.title('CO2 and PetCO2')
-    plt.plot(co2, '-', petco2, '-')
-    plt.savefig(f'{outname}_petco2.png', dpi=SET_DPI)
-    plt.close()
-
-    # Demean and export
-    petco2 = petco2 - petco2.mean()
-    np.savetxt(f'{outname}_petco2.1D', petco2, fmt='%.18f')
-
-    # Convolve, and then rescale to have same amplitude (?)
-    co2_conv = np.convolve(petco2, hrf)
-    co2_conv = np.interp(co2_conv, (co2_conv.min(), co2_conv.max()),
-                         (petco2.min(), petco2.max()))
-
-    plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-    plt.title('PetCO2 and convolved regressor (PetCO2hrf)')
-    plt.plot(co2_conv, '-', petco2, '-')
-    plt.savefig(f'{outname}_petco2hrf.png', dpi=SET_DPI)
-    plt.close()
-
-    np.savetxt(f'{outname}_petco2hrf.1D', co2_conv, fmt='%.18f')
-
-    return co2_conv
 
 
 def x_corr(func, co2, lastrep, firstrep=0, offset=0):
@@ -339,7 +226,7 @@ def phys2cvr(fname_func, fname_co2='', fname_pidx='', fname_mask='', outdir='',
             func_avg = np.genfromtxt(fname_func)
             if apply_filter:
                 LGR.info('Applying butterworth filter to {fname_func}')
-                func_avg = filter_signal(func_avg, tr, lowcut, highcut)
+                func_avg = signal.filter_signal(func_avg, tr, lowcut, highcut)
         else:
             raise Exception('Provided functional signal, but no TR specified! '
                             'Rerun specifying the TR')
@@ -365,7 +252,7 @@ def phys2cvr(fname_func, fname_co2='', fname_pidx='', fname_mask='', outdir='',
 
         if apply_filter:
             LGR.info('Obtaining filtered average of {fname_func}')
-            func_filt = filter_signal(func, tr, lowcut, highcut)
+            func_filt = signal.filter_signal(func, tr, lowcut, highcut)
             func_avg = func_filt[mask].mean(axis=-1)
         else:
             func_avg = func[mask].mean(axis=-1)
@@ -424,7 +311,7 @@ def phys2cvr(fname_func, fname_co2='', fname_pidx='', fname_mask='', outdir='',
         # Set output file & path - calling splitext twice cause .gz
         basename_co2 = os.path.splitext(os.path.splitext(os.path.basename(fname_co2))[0])[0]
         outname = os.join(outdir, basename_co2)
-        petco2hrf = convolve_petco2(co2, pidx, freq, outname)
+        petco2hrf = signal.convolve_petco2(co2, pidx, freq, outname)
 
     regr = get_regr(func_avg, petco2hrf, tr, freq, outname, maxlag, trial_len,
                     n_trials, no_pad, '.1D', lagged_regression)
