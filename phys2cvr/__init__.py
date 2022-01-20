d61f10eb3d955bd194d84091ba5fe9f60c77fb67
Stefano Moia <s.moia@bcbl.eu>
2022-01-20 14:30:31 +0100

    Add full import

--- phys2cvr/__init__.py
+++ phys2cvr/__init__.py
@@ -1,4 +1,15 @@
+"""Hopefully importing everything."""
+
+import pkgutil
 
 from ._version import get_versions
+
+
 __version__ = get_versions()['version']
 del get_versions
+
+__all__ = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
