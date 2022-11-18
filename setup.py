2398cfaced872e31b91f776f7f1a8d6da209c08f
smoia <s.moia.research@gmail.com>
2022-11-18 13:37:22 +0100

    Update setuptool security

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 import versioneer
 
-SETUP_REQUIRES = ["setuptools >= 30.3.0"]
-SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
+SETUP_REQUIRES = ['setuptools >= 65.5.1']
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
 if __name__ == "__main__":
     setup(
