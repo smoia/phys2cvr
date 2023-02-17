3e1951e071fe13bcd3c2c564f8f053e8f569e1b4
smoia <s.moia.research@gmail.com>
2023-02-17 21:31:31 +0100

    Convolution should stay "full", but be cropped at the right moment

--- phys2cvr/signal.py
+++ phys2cvr/signal.py
@@ -167,7 +167,7 @@ def compute_petco2hrf(co2, pidx, freq, outname, response_function="hfr", mode="f
             from phys2denoise.metrics.responses import rrf
         except ImportError:
             raise ImportError(
-                "phys2denoise requires the use of RRF response functions. "
+                "phys2denoise is required for the use of RRF response functions. "
                 "Please see install instructions."
             )
         hrf = rrf
@@ -176,7 +176,7 @@ def compute_petco2hrf(co2, pidx, freq, outname, response_function="hfr", mode="f
             from phys2denoise.metrics.responses import crf
         except ImportError:
             raise ImportError(
-                "phys2denoise requires the use of CRF response functions. "
+                "phys2denoise required for the use of CRF response functions. "
                 "Please see install instructions."
             )
         hrf = crf
