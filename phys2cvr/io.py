653d5701df6bbf4f45e3a88b53c259df0767102c
smoia <s.moia.research@gmail.com>
2023-03-16 15:32:49 +0100

    Resample signal is now based on samples by default

--- phys2cvr/io.py
+++ phys2cvr/io.py
@@ -220,7 +220,7 @@ def export_regressor(petco2hrf_shift, ntp, outname, suffix="petco2hrf", ext=".1D
     petco2hrf_demean : np.ndarray
         Interpolated version of `petco2hrf_shift` in the sampling of the fMRI data.
     """
-    petco2hrf_shift = signal.resample_signal_samples(petco2hrf_shift, ntp)
+    petco2hrf_shift = signal.resample_signal(petco2hrf_shift, ntp)
     petco2hrf_demean = petco2hrf_shift - petco2hrf_shift.mean(axis=-1)
     if petco2hrf_demean.ndim > 1:
         for i in range(petco2hrf_demean.shape[-1]):
