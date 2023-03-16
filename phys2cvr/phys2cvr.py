f1c72594c47e9b713ff6d3357970db7e9454b90e
smoia <s.moia.research@gmail.com>
2023-03-16 22:17:20 +0100

    Fix regressor shift matrix orientation

--- phys2cvr/phys2cvr.py
+++ phys2cvr/phys2cvr.py
@@ -713,8 +713,8 @@ def phys2cvr(
                 for n, i in enumerate(lag_range):
                     LGR.info(f'Perform L-GLM number {n + 1} of {len(lag_range)}')
                     try:
-                        regr = regr_shifts[:, i]
-                        LGR.debug(f'Using shift {i} from matrix in memory: {regr}')
+                        regr = regr_shifts[i, :]
+                        LGR.debug(f"Using shift {i} from matrix in memory: {regr}")
                     except NameError:
                         regr = np.genfromtxt(f'{outprefix}_{i:04g}')
                         LGR.debug(f'Reading shift {i} from file {outprefix}_{i:04g}')
