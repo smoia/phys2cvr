bed6d3866bf3959dee7d25f5d8bced179a3c7de0
Stefano Moia <s.moia@bcbl.eu>
2023-08-28 20:29:47 +0200

    fix: Do not crawl tests folder when importing modules

--- phys2cvr/__init__.py
+++ phys2cvr/__init__.py
@@ -9,6 +9,7 @@ del get_versions
 
 __all__ = []
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+    if "tests" not in module_name:
+        __all__.append(module_name)
+        _module = loader.find_module(module_name).load_module(module_name)
+        globals()[module_name] = _module
