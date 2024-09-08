a74bb76c448fbe4feca6401d7743caab199819e7
Kristina Zvolanek <kristinazvolanek@u.northwestern.edu>
2024-09-08 00:49:03 -0500

    fix indent

--- phys2cvr/__init__.py
+++ phys2cvr/__init__.py
@@ -12,9 +12,9 @@ __all__ = []
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
 
-     try:
+    try:
         _module = loader.find_module(module_name).load_module(module_name)
-     except AttributeError:
+    except AttributeError:
         _module = loader.find_spec(module_name).load_module(module_name)
     globals()[module_name] = _module
 
