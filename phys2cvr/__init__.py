4d57d2b8bfb96971d17c93d1c83bf57d5048e8d2
Kristina Zvolanek <kristinazvolanek@u.northwestern.edu>
2024-09-08 00:52:52 -0500

    change import of get_versions to absolute

--- phys2cvr/__init__.py
+++ phys2cvr/__init__.py
@@ -2,12 +2,6 @@
 
 import pkgutil
 
-from ._version import get_versions
-
-
-__version__ = get_versions()['version']
-del get_versions
-
 __all__ = []
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
@@ -18,5 +12,6 @@ for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
         _module = loader.find_spec(module_name).loader.load_module(module_name)
     globals()[module_name] = _module
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
